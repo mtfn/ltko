f7234ab021868952b3831831e6795d2f4c9dabf7
mtfn <48189055+mtfn@users.noreply.github.com>
2026-03-19 16:58:23 -0700

    final

--- data.js
+++ data.js
@@ -1,62 +1,122 @@
 const FLASHCARD_DATA = [
     {
-        "text": "요즘 날씨가 참 따뜻하고 좋네요. ",
+        "text": "저는 서울에서 태어나서 자랐어요.",
         "lang": "ko",
         "id": 0,
         "audio_src": "audio_files/audio_0.mp3"
     },
     {
-        "text": "이번 주말에 전주 한옥 마을에 가려고 해요.",
+        "text": "부모님과 형제들도 서울에 사세요?",
         "lang": "ko",
         "id": 1,
         "audio_src": "audio_files/audio_1.mp3"
     },
     {
-        "text": "전주가 서울에서 멀지요?",
+        "text": "형은 취업해서 부산에 살아요.",
         "lang": "ko",
         "id": 2,
         "audio_src": "audio_files/audio_2.mp3"
     },
     {
-        "text": "시험이 있어서 도서관에서 공부하려고 해요.",
+        "text": "남동생이 두 명 있어요. 저는 첫째예요.",
         "lang": "ko",
         "id": 3,
         "audio_src": "audio_files/audio_3.mp3"
     },
     {
-        "text": "시험이 많네요. 그럼 열심히 공부하세요.",
+        "text": "둘째 남동생은 초등학생이에요.",
         "lang": "ko",
         "id": 4,
         "audio_src": "audio_files/audio_4.mp3"
     },
     {
-        "text": "등산 가려고 했는데 비가 많이 와서 못 갔어요.",
+        "text": "저는 영국에 가서 가족들을 만날 거예요.",
         "lang": "ko",
         "id": 5,
         "audio_src": "audio_files/audio_5.mp3"
     },
     {
-        "text": "집에서 청소하고 쉬었어요.",
+        "text": "학교에서 멀어서 가까운 곳으로 이사하려고 해요.",
         "lang": "ko",
         "id": 6,
         "audio_src": "audio_files/audio_6.mp3"
     },
     {
-        "text": "부산은 날씨가 좋아서 구경을 많이 했어요.",
+        "text": "기숙사에서는 방을 혼자 쓰세요?",
         "lang": "ko",
         "id": 7,
         "audio_src": "audio_files/audio_7.mp3"
     },
     {
-        "text": "부산에서 어느 곳을 추천해요?",
+        "text": "룸메이트하고 같이 방을 쓸 거예요.",
         "lang": "ko",
         "id": 8,
         "audio_src": "audio_files/audio_8.mp3"
     },
     {
-        "text": "해운대하고 자갈치 시장에 꼭 가 보세요.",
+        "text": "기숙사에 이사 와서 연락 주세요.",
         "lang": "ko",
         "id": 9,
         "audio_src": "audio_files/audio_9.mp3"
+    },
+    {
+        "text": "그림 그리는 것이 제 취미예요.",
+        "lang": "ko",
+        "id": 10,
+        "audio_src": "audio_files/audio_10.mp3"
+    },
+    {
+        "text": "시간이 있을 때 보통 뭐 해요?",
+        "lang": "ko",
+        "id": 11,
+        "audio_src": "audio_files/audio_11.mp3"
+    },
+    {
+        "text": "여행을 가서 사진을 많이 찍어요.",
+        "lang": "ko",
+        "id": 12,
+        "audio_src": "audio_files/audio_12.mp3"
+    },
+    {
+        "text": "제주도에 여행 갔을 때 한라산에서 찍었어요.",
+        "lang": "ko",
+        "id": 13,
+        "audio_src": "audio_files/audio_13.mp3"
+    },
+    {
+        "text": "네, 저는 어렸을 때 가 봤어요.",
+        "lang": "ko",
+        "id": 14,
+        "audio_src": "audio_files/audio_14.mp3"
+    },
+    {
+        "text": "제가 한국 음식을 만들 수 있어요.",
+        "lang": "ko",
+        "id": 15,
+        "audio_src": "audio_files/audio_15.mp3"
+    },
+    {
+        "text": "불고기와 떡볶이를 만들게요.",
+        "lang": "ko",
+        "id": 16,
+        "audio_src": "audio_files/audio_16.mp3"
+    },
+    {
+        "text": "오늘 한국어 수업에서 친구들한테 물어봐요.",
+        "lang": "ko",
+        "id": 17,
+        "audio_src": "audio_files/audio_17.mp3"
+    },
+    {
+        "text": "그레이스 씨는 테니스 치는 걸 좋아하니까 같이 테니스를 쳐요.",
+        "lang": "ko",
+        "id": 18,
+        "audio_src": "audio_files/audio_18.mp3"
+    },
+    {
+        "text": "한국어 반 친구들을 초대하고 싶은데 다음 주 월요일에 시험이 있어요.",
+        "lang": "ko",
+        "id": 19,
+        "audio_src": "audio_files/audio_19.mp3"
     }
 ];
