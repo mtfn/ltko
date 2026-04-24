c57e19e4116e51d7de1340b2b8d9e78b0720f23e
mtfn <48189055+mtfn@users.noreply.github.com>
2026-04-24 13:40:53 -0700

    1c test 2

--- data.js
+++ data.js
@@ -1,60 +1,60 @@
 const FLASHCARD_DATA = [
     {
-        "text": "잘 지내는데 요즘 좀 바빠요.",
+        "text": "머리가 아프고 열이 나요.",
         "lang": "ko",
         "id": 0,
         "audio_src": "audio_files/audio_0.mp3"
     },
     {
-        "text": "요즘 일이 많아서 스트레스가 많아요.",
+        "text": "언제부터 증상이 있었어요?",
         "lang": "ko",
         "id": 1,
         "audio_src": "audio_files/audio_1.mp3"
     },
     {
-        "text": "프로젝트가 끝나면 좀 쉬고 싶어요.",
+        "text": "먼저 체온을 재 보겠습니다",
         "lang": "ko",
         "id": 2,
         "audio_src": "audio_files/audio_2.mp3"
     },
     {
-        "text": "저는 운전하면서 노래를 크게 불러요.",
+        "text": "식사하신 후에 약을 복용하세요.",
         "lang": "ko",
         "id": 3,
         "audio_src": "audio_files/audio_3.mp3"
     },
     {
-        "text": "좋은 방법이네요. 스트레스가 잘 풀리겠어요.",
+        "text": "증상이 없어지면 병원에 안 오셔도 됩니다.",
         "lang": "ko",
         "id": 4,
         "audio_src": "audio_files/audio_4.mp3"
     },
     {
-        "text": "스트레스를 받으면 공원에서 테니스를 쳐요.",
+        "text": "소화가 안 되고 배가 많이 아파요.",
         "lang": "ko",
         "id": 5,
         "audio_src": "audio_files/audio_5.mp3"
     },
     {
-        "text": "지난 학기가 너무 힘들어서 방학동안 쉬었어.",
+        "text": "소화제도 먹었는데 효과가 없네요.",
         "lang": "ko",
         "id": 6,
         "audio_src": "audio_files/audio_6.mp3"
     },
     {
-        "text": "커피숍에서 아르바이트를 시작했어.",
+        "text": "소화가 안 되는데 참으면 안 돼요.",
         "lang": "ko",
         "id": 7,
         "audio_src": "audio_files/audio_7.mp3"
     },
     {
-        "text": "영화도 많이 보고 책도 여러 권 읽었어.",
+        "text": "수업은 걱정 안 해도 돼요.",
         "lang": "ko",
         "id": 8,
         "audio_src": "audio_files/audio_8.mp3"
     },
     {
-        "text": "스칼렛이 대학원에 합격을 했대.",
+        "text": "병원에 다녀온 후에 푹 쉬세요.",
         "lang": "ko",
         "id": 9,
         "audio_src": "audio_files/audio_9.mp3"
