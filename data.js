fa68ef94b00b4999a2fb3813d581b060e3ebbc80
mtfn <48189055+mtfn@users.noreply.github.com>
2026-05-12 10:56:22 -0700

    l13

--- data.js
+++ data.js
@@ -1,60 +1,60 @@
 const FLASHCARD_DATA = [
     {
-        "text": "머리가 아프고 열이 나요.",
+        "text": "한국어 반 친구들이 많이 가는 것 같아요.",
         "lang": "ko",
         "id": 0,
         "audio_src": "audio_files/audio_0.mp3"
     },
     {
-        "text": "언제부터 증상이 있었어요?",
+        "text": "한국에 온 동생하고 여행을 가요.",
         "lang": "ko",
         "id": 1,
         "audio_src": "audio_files/audio_1.mp3"
     },
     {
-        "text": "먼저 체온을 재 보겠습니다",
+        "text": "하얀 색처럼 밝은 색이 어울리는 편이에요.",
         "lang": "ko",
         "id": 2,
         "audio_src": "audio_files/audio_2.mp3"
     },
     {
-        "text": "식사하신 후에 약을 복용하세요.",
+        "text": "키가 큰 편이어서 굽이 낮은 신발이 어울려요.",
         "lang": "ko",
         "id": 3,
         "audio_src": "audio_files/audio_3.mp3"
     },
     {
-        "text": "증상이 없어지면 병원에 안 오셔도 됩니다.",
+        "text": "추천해 준 스타일이 정말 마음에 들어요.",
         "lang": "ko",
         "id": 4,
         "audio_src": "audio_files/audio_4.mp3"
     },
     {
-        "text": "소화가 안 되고 배가 많이 아파요.",
+        "text": "지난 봄 방학에 제주도에서 찍은 사진이에요.",
         "lang": "ko",
         "id": 5,
         "audio_src": "audio_files/audio_5.mp3"
     },
     {
-        "text": "소화제도 먹었는데 효과가 없네요.",
+        "text": "파란 모자를 쓰고 까만 셔츠를 입은 사람이에요.",
         "lang": "ko",
         "id": 6,
         "audio_src": "audio_files/audio_6.mp3"
     },
     {
-        "text": "소화가 안 되는데 참으면 안 돼요.",
+        "text": "보통 혼자 여행을 하는 편이에요.",
         "lang": "ko",
         "id": 7,
         "audio_src": "audio_files/audio_7.mp3"
     },
     {
-        "text": "수업은 걱정 안 해도 돼요.",
+        "text": "제주도에 유명한 음식이 많아서 많이 먹었어요.",
         "lang": "ko",
         "id": 8,
         "audio_src": "audio_files/audio_8.mp3"
     },
     {
-        "text": "병원에 다녀온 후에 푹 쉬세요.",
+        "text": "이번에는 한국어 반 친구들과 같이 갔어요.",
         "lang": "ko",
         "id": 9,
         "audio_src": "audio_files/audio_9.mp3"
