9706f890f3bb8ffc45a5f7f546f7c0a125b10553
mtfn <48189055+mtfn@users.noreply.github.com>
2026-06-10 09:51:10 -0700

    final final

--- data.js
+++ data.js
@@ -1,62 +1,122 @@
 const FLASHCARD_DATA = [
     {
-        "text": "한국어 반 친구들이 많이 가는 것 같아요.",
+        "text": "소포를 어디로 보내실 겁니까?",
         "lang": "ko",
         "id": 0,
         "audio_src": "audio_files/audio_0.mp3"
     },
     {
-        "text": "한국에 온 동생하고 여행을 가요.",
+        "text": "보통 우편으로는 일주일이 걸립니다.",
         "lang": "ko",
         "id": 1,
         "audio_src": "audio_files/audio_1.mp3"
     },
     {
-        "text": "하얀 색처럼 밝은 색이 어울리는 편이에요.",
+        "text": "미국 샌디에이고로 보내고 싶은데 얼마나 걸릴까요?",
         "lang": "ko",
         "id": 2,
         "audio_src": "audio_files/audio_2.mp3"
     },
     {
-        "text": "키가 큰 편이어서 굽이 낮은 신발이 어울려요.",
+        "text": "소포 안에 깨지는 물건이 있습니까?",
         "lang": "ko",
         "id": 3,
         "audio_src": "audio_files/audio_3.mp3"
     },
     {
-        "text": "추천해 준 스타일이 정말 마음에 들어요.",
+        "text": "체크 카드를 만들고 싶습니다.",
         "lang": "ko",
         "id": 4,
         "audio_src": "audio_files/audio_4.mp3"
     },
     {
-        "text": "지난 봄 방학에 제주도에서 찍은 사진이에요.",
+        "text": "먼저 은행 계좌를 만드셔야 됩니다.",
         "lang": "ko",
         "id": 5,
         "audio_src": "audio_files/audio_5.mp3"
     },
     {
-        "text": "파란 모자를 쓰고 까만 셔츠를 입은 사람이에요.",
+        "text": "외국인이시니까 여권이 필요합니다.",
         "lang": "ko",
         "id": 6,
         "audio_src": "audio_files/audio_6.mp3"
     },
     {
-        "text": "보통 혼자 여행을 하는 편이에요.",
+        "text": "이 신청서를 작성해 주십시오.",
         "lang": "ko",
         "id": 7,
         "audio_src": "audio_files/audio_7.mp3"
     },
     {
-        "text": "제주도에 유명한 음식이 많아서 많이 먹었어요.",
+        "text": "마지막에 서명해 주십시오.",
         "lang": "ko",
         "id": 8,
         "audio_src": "audio_files/audio_8.mp3"
     },
     {
-        "text": "이번에는 한국어 반 친구들과 같이 갔어요.",
+        "text": "체크카드를 오늘 바로 받을 수 있을까요?",
         "lang": "ko",
         "id": 9,
         "audio_src": "audio_files/audio_9.mp3"
+    },
+    {
+        "text": "가족들한테 줄 선물을 검색하고 있어요.",
+        "lang": "ko",
+        "id": 10,
+        "audio_src": "audio_files/audio_10.mp3"
+    },
+    {
+        "text": "버스를 타고 안국역에서 내려서 걸어가면 돼요.",
+        "lang": "ko",
+        "id": 11,
+        "audio_src": "audio_files/audio_11.mp3"
+    },
+    {
+        "text": "그럼 저는 지하철을 타는게 좋겠어요.",
+        "lang": "ko",
+        "id": 12,
+        "audio_src": "audio_files/audio_12.mp3"
+    },
+    {
+        "text": "유나 씨, 추천하고 싶은 공예품이 있어요.",
+        "lang": "ko",
+        "id": 13,
+        "audio_src": "audio_files/audio_13.mp3"
+    },
+    {
+        "text": "부채나 찻잔 세트나 수저 세트 어때요?",
+        "lang": "ko",
+        "id": 14,
+        "audio_src": "audio_files/audio_14.mp3"
+    },
+    {
+        "text": "새로 생긴 학교 앞 음식점에 가 본 적이 있어요?",
+        "lang": "ko",
+        "id": 15,
+        "audio_src": "audio_files/audio_15.mp3"
+    },
+    {
+        "text": "음식이 아주 맛있고 서비스도 좋아요.",
+        "lang": "ko",
+        "id": 16,
+        "audio_src": "audio_files/audio_16.mp3"
+    },
+    {
+        "text": "테이블이 여섯 개 밖에 없어서 오래 기다렸어요.",
+        "lang": "ko",
+        "id": 17,
+        "audio_src": "audio_files/audio_17.mp3"
+    },
+    {
+        "text": "오늘 가려고 하는데 일찍 가는게 좋겠네요.",
+        "lang": "ko",
+        "id": 18,
+        "audio_src": "audio_files/audio_18.mp3"
+    },
+    {
+        "text": "아르바이트를 하거나 자격증 시험 공부를 할 거예요.",
+        "lang": "ko",
+        "id": 19,
+        "audio_src": "audio_files/audio_19.mp3"
     }
 ];
